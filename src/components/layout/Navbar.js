67388f881d13d5a9963b155ca06d449073283d8d
asherzod1 <sh.abdumutalov1@gmail.com>
2022-03-05 20:07:35 +0500

    second commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import '../../style/navbar.scss'
+import {RiNotificationLine} from 'react-icons/ri'
+function Navbar(props) {
+    const [toggle, setTogle] = useState(false)
+    const switch_toggle = () =>{
+        setTogle(!toggle)
+    }
+    return (
+        <div className="navbarr">
+            <div className="navbarr__left">
+                <h3 className="navbarr__left__title">Zako iT academy</h3>
+            </div>
+            <div className="navbarr__right">
+                <div className="navbarr__right__left">
+                    <input className="navbarr__input" type="text"/>
+                </div>
+                <div className="navbarr__right__right">
+                    <div className="open_for_desktop" style={{marginRight:'20px'}}>
+                        <p>Open for Desktop</p>
+                    </div>
+                    <div style={{marginRight:'23px'}}>
+                        <label className="switch" >
+                            <input  type={toggle ? "checkbox": ''} checked onClick={()=>{switch_toggle()}}/>
+                            <span className="slider round"></span>
+                        </label>
+                    </div>
+                    <div style={{fontSize:'20px'}}>
+                        <RiNotificationLine />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
