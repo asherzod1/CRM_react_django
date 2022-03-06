43f990ebf2ef011fba16cd132e89935a4a453ae8
sherzod <otaqulovsherzod2503@gmail.com>
2022-03-06 13:11:09 +0500

    'KNKN'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Layout from "./components/layout";
+import Layout from "./components/layout/Layout";
+import CoursePage from "./components/pages/CoursePage";
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <CoursePage/>
     </div>
   );
 }
